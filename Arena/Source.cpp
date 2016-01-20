838727c589d2438acf01aa44b6129697e0a756bf
Cameron Conradt <cameron@thecconradts.com>
2016-01-20 11:44:53 -0700

    Adding Source Functions

--- Arena/Source.cpp
+++ Arena/Source.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <sstream>
 #include <fstream>
-#include "FighterInterface.h"
+#include <set>
+#include <iomanip>
+/*#include "FighterInterface.h"
 #include "Factory.h"
 #include "ArenaInterface.h"
 #include "Arena.h"
@@ -10,24 +12,66 @@
 #include "Cleric.h"
 #include "Robot.h"
 #include "Archer.h"
-
+*/
 using namespace std;
+
+void addFighter(set<string> Fighters, string input);
+bool correct(string input);
 
 int main()
 {
-	vector<Fighter> Fighters;
-	stringstream input;
-	getline(cin,input)
+	set<string> Fighters;
+	string input;
+	while (getline(cin, input))
+	{
+		addFighter(Fighters, input);
+	}
 
 
 }
 //Adds Fighter to the vector
-void addFighter()
+void addFighter(set<string> Fighters, string input)
 {
+	stringstream ss;
+	string name;
+	char type;
+	int hitpoints;
+	int strength;
+	int speed;
+	int magic;
+	if (correct(input))
+	{
+		ss << input;
+		ss >> name;
+		ss >> type;
+		ss >> hitpoints;
+		ss >> strength;
+		ss >> speed;
+		ss >> magic;
+	}
 
 }
 //Determines if input is correct
-bool check()
+bool correct(string input)
 {
+	stringstream ss;
+	string name;
+	char type;
+	int hitpoints;
+	int strength;
+	int speed;
+	int magic;
+	ss << input;
+	ss >> name;
+	ss >> type;
+	if (type != ("C" || "R" || "A"))
+		return false;
+	ss >> hitpoints;
+	ss >> strength;
+	ss >> speed;
+	ss >> magic;
+	if ((hitpoints || strength || speed || magic) < 0)
+		return false;
+	return true;
 
 }
